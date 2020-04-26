46927f48c26ac3c19958472bfc9bd02ce938440b
Jacek Spolnik <jacek@revolut.com>
2020-04-26 15:42:07 +0200

    adding first version

---
+++ contracts/Fundraiser.sol
@@ -0,0 +1,33 @@
+pragma solidity >0.6.0 < 0.7.0;
+
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+
+contract Fundraiser is Ownable {
+
+    string public name;
+    string public url;
+    string public imageURL;
+    string public description;
+
+    address payable public beneficiary;
+
+    constructor(
+        string memory _name,
+        string memory _url,
+        string memory _imageURL,
+        string memory _description,
+        address payable _beneficiary,
+        address _custodian
+    ) public {
+        name = _name;
+        url = _url;
+        imageURL = _imageURL;
+        description = _description;
+        beneficiary = _beneficiary;
+        transferOwnership(_custodian);
+    }
+
+    function setBeneficiary(address payable _beneficiary) public onlyOwner {
+        beneficiary = _beneficiary;
+    }
+}
